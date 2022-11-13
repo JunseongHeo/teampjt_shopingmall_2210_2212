c806953a968895d381344bb51e4c0634dcf5c872
JunseongHeo <oksk476@naver.com>
2022-11-13 12:59:26 +0900

    스프링 웹 개발 기초

--- src/main/java/common/controller/HelloController.java
+++ src/main/java/common/controller/HelloController.java
@@ -4,6 +4,7 @@ package common.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -14,5 +15,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
